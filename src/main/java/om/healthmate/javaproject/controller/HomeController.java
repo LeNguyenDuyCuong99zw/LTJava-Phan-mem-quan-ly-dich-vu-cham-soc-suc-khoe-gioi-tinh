8a6556c43415f14fd4451ba57d8ec558ae320677
Tuấn Anh <dangtruongtuananh0308@gmail.com>
2025-06-22 20:46:13 +0700

    Tuan anh commit tất cả file java, css của support,video,html

--- src/main/java/om/healthmate/javaproject/controller/HomeController.java
+++ src/main/java/om/healthmate/javaproject/controller/HomeController.java
@@ -2,7 +2,7 @@ package om.healthmate.javaproject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
@@ -10,4 +10,44 @@ public class HomeController {
     public String home() {
         return "index";
     }
+
+
+    @RequestMapping({
+        "/s_experiences",
+    })
+    public String s_exper(){
+        return "pages/s_experiences"; 
+    }
+    @GetMapping("/page_s_1")
+    public String pageS1() {
+        return "pages/page_s_1";
+    }
+    @GetMapping("/page_s_2")
+    public String pageS2() {
+        return "pages/page_s_2";
+    }
+    @GetMapping("/page_s_3")
+    public String pageS3() {
+        return "pages/page_s_3";
+    }
+    @GetMapping("/page_s_4")
+    public String pageS4() {
+        return "pages/page_s_4";
+    }
+    @GetMapping("/page_s_5")
+    public String pageS5() {
+        return "pages/page_s_5";
+    }
+
+
+    
+    @GetMapping("/health-records")
+    public String healthRecords(){
+        return "pages/health-records"; 
+    }
+
+    //@GetMapping("/appointment-booking")
+    //public String appointmentBooking() {
+    //    return "pages/appointment-booking";
+    //}
 }
