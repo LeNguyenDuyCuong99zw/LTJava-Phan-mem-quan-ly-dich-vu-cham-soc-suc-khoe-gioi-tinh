0cc0d94c45d2004a1d575d86faec885e0c68fb73
Duy Cường <duycuong9897@gmail.com>
2025-07-02 11:08:36 +0700

    Le Nguyen Duy Cuong commit reponsitory của hoi dap cau hỏi

--- src/main/java/om/healthmate/javaproject/repository/UserRepository.java
+++ src/main/java/om/healthmate/javaproject/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package om.healthmate.javaproject.repository;
 
-import om.healthmate.javaproject.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import om.healthmate.javaproject.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
-} 
+    java.util.List<User> findByRole(String role);
+}
